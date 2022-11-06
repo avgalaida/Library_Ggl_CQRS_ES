b22c8e3cfb6b6724fb83542dc874c1e6634ef4da
Alexandr Galaida <evaAxelrod@yandex.ru>
2022-11-06 09:29:00 +0500

    stable 1.0

--- backend/Graphql/Mutation.cs
+++ backend/Graphql/Mutation.cs
@@ -6,6 +6,44 @@ namespace backend.Graphql;
 
 public class Mutation
 {
+    public async Task<Book> RollbackBook([Service] AppDbContext context, 
+        [Service] ITopicEventSender sender, RollbackBookEvent eEvent )
+    {
+        var book = new Book(await GetAggregate(context, eEvent.AggregateId));
+        
+        eEvent.Register(book, eEvent);
+
+        await UpgradeAggregateLastRevision(context, book.Id);
+        await InsertEvent(context, eEvent);
+            
+        await sender.SendAsync(nameof(Subscription.OnBookRollbacked), eEvent);
+
+        return book;
+    }
+    private static async Task<BaseAggregate> InsertAggregate([Service] AppDbContext context, BaseAggregate aggregate)
+    {
+        context.BaseAggregate.Add(aggregate);
+        await context.SaveChangesAsync();   
+        return aggregate;
+    }
+    private static async Task<BaseAggregate> GetAggregate([Service] AppDbContext context, string id)
+    {
+        var aggregate = await context.BaseAggregate.FirstOrDefaultAsync(a => a.Id == id);
+        return aggregate;
+    }
+    private  static async Task<BaseAggregate> UpgradeAggregateLastRevision([Service] AppDbContext context, string id)
+    {
+        var aggregate = await context.BaseAggregate.FirstOrDefaultAsync(a => a.Id == id);
+        aggregate.LastRevision++;
+        await context.SaveChangesAsync();
+        return aggregate;
+    }
+    private static async Task<BaseEvent> InsertEvent([Service] AppDbContext context, BaseEvent eEvent)
+    {
+        context.BaseEvent.Add(eEvent);
+        await context.SaveChangesAsync();
+        return eEvent;
+    }
     public async Task<Book> CreateBook([Service] AppDbContext context,
         [Service] ITopicEventSender sender, CreateBookEvent eEvent)
     {
@@ -82,44 +120,5 @@ public class Mutation
         await sender.SendAsync(nameof(Subscription.OnBookAuthorsChanged), eEvent);
 
         return book;
-    }
-    
-    public async Task<Book> RollbackBook([Service] AppDbContext context, 
-        [Service] ITopicEventSender sender, RollbackBookEvent eEvent )
-    {
-        var book = new Book(await GetAggregate(context, eEvent.AggregateId));
-        
-        eEvent.Register(book, eEvent);
-
-        await UpgradeAggregateLastRevision(context, book.Id);
-        await InsertEvent(context, eEvent);
-            
-        await sender.SendAsync(nameof(Subscription.OnBookRollbacked), eEvent);
-
-        return book;
-    }
-    private static async Task<BaseAggregate> InsertAggregate([Service] AppDbContext context, BaseAggregate aggregate)
-    {
-        context.BaseAggregate.Add(aggregate);
-        await context.SaveChangesAsync();   
-        return aggregate;
-    }
-    private static async Task<BaseAggregate> GetAggregate([Service] AppDbContext context, string id)
-    {
-        var aggregate = await context.BaseAggregate.FirstOrDefaultAsync(a => a.Id == id);
-        return aggregate;
-    }
-    private  static async Task<BaseAggregate> UpgradeAggregateLastRevision([Service] AppDbContext context, string id)
-    {
-        var aggregate = await context.BaseAggregate.FirstOrDefaultAsync(a => a.Id == id);
-        aggregate.LastRevision++;
-        await context.SaveChangesAsync();
-        return aggregate;
-    }
-    private static async Task<BaseEvent> InsertEvent([Service] AppDbContext context, BaseEvent eEvent)
-    {
-        context.BaseEvent.Add(eEvent);
-        await context.SaveChangesAsync();
-        return eEvent;
     }
 }
